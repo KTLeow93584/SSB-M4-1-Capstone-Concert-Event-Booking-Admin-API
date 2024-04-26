cabad59d265e12b366ce6f38756950f3455088b4
Leow Kean Tat <leowkeantat@gmail.com>
2024-04-26 13:44:05 +0800

    Increased the maximum file limit for JSON file uploads. (~50MB)

--- src/index.js
+++ src/index.js
@@ -13,7 +13,7 @@ const {
 // ======================================
 let app = express();
 app.use(cors());
-app.use(express.json());
+app.use(express.json({limit: '50mb'}));
 
 const pool = new Pool({
   connectionString: DATABASE_URL,
@@ -86,7 +86,7 @@ app.use((req, res, next) => {
 //https://www.npmjs.com/package/memory-cache-node
 // ======================================
 var bodyParser = require('body-parser');
-app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.urlencoded({ limit: '50mb', extended: true }));
 // =======================================
 // Sets the view engine to ejs
 app.set("view engine", 'ejs');
